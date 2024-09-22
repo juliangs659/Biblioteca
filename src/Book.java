65ce18ed3ad7fecb339ef5817f4458b22f31bf40
Julian Gutierrez <julian_agutierrezs@soy.sena.edu.co>
2024-09-22 17:08:37 -0500

    Primer commit

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,87 @@
+import java.util.Date;
+
 public class Book {
+    private Long id;
+    private String titulo;
+    private String autor;
+    private String categoria;
+    private String contenido; // Puede ser la URL al recurso
+    private Date fechaPublicacion;
+    private String descripcion;
+    private int cantidadPaginas;
+    private String lenguaje;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getTitulo() {
+    return titulo;
+  }
+
+  public void setTitulo(String titulo) {
+    this.titulo = titulo;
+  }
+
+  public String getAutor() {
+    return autor;
+  }
+
+  public void setAutor(String autor) {
+    this.autor = autor;
+  }
+
+  public String getCategoria() {
+    return categoria;
+  }
+
+  public void setCategoria(String categoria) {
+    this.categoria = categoria;
+  }
+
+  public String getContenido() {
+    return contenido;
+  }
+
+  public void setContenido(String contenido) {
+    this.contenido = contenido;
+  }
+
+  public Date getFechaPublicacion() {
+    return fechaPublicacion;
+  }
+
+  public void setFechaPublicacion(Date fechaPublicacion) {
+    this.fechaPublicacion = fechaPublicacion;
+  }
+
+  public String getDescripcion() {
+    return descripcion;
+  }
+
+  public void setDescripcion(String descripcion) {
+    this.descripcion = descripcion;
+  }
+
+  public int getCantidadPaginas() {
+    return cantidadPaginas;
+  }
+
+  public void setCantidadPaginas(int cantidadPaginas) {
+    this.cantidadPaginas = cantidadPaginas;
+  }
+
+  public String getLenguaje() {
+    return lenguaje;
+  }
+
+  public void setLenguaje(String lenguaje) {
+    this.lenguaje = lenguaje;
+  }
 }
+
+
--- src/User.java
+++ src/User.java
@@ -1,4 +1,48 @@
-package PACKAGE_NAME;
-
 public class User {
+  private Long id;
+  private String nombre;
+  private String email;
+  private String rol; // Admin, Usuario Regular, etc.
+  private String password;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getNombre() {
+    return nombre;
+  }
+
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getRol() {
+    return rol;
+  }
+
+  public void setRol(String rol) {
+    this.rol = rol;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
 }
+
